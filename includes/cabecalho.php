da87ca40b133b85202dc20de1e10b9781f6a9f4c
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2026-01-15 15:33:01 -0300

    inserindo informações diversas no rodapé

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="PT-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-
-<body>
-
-
-    <header id="cabecalho">
+<header id="cabecalho">
         <section id="logo_e_icones">
             <div id="menu"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg></div>
             <a id="logo_link" href=""><img id="logo" src="media/img/logo.webp" alt=""></a>
@@ -93,7 +80,3 @@
             </a>
         </section>
     </header>
-
-</body>
-
-</html>
