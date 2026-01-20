98df46f22113f143becbe6d13d70976edb6f6a47
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2026-01-20 12:42:01 -0300

    faz html e estilização do painel administrativo

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,32 @@
 <?php
-
-require_once 'autenticacao.php';
+// require_once 'autenticacao.php';
 ?>
 
-<h1>Bem-vindo ao Painel, <?php echo htmlspecialchars($_SESSION['admin_nome']); ?></h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Top Calçados - Painel Administrativo</title>
+</head>
+
+<body style="height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center; ">
+    <h1 id="titulo_painel">Bem-vindo ao Painel,
+        <?php #echo htmlspecialchars($_SESSION['admin_nome']); ?>
+    </h1>
+    <nav id="menu_admin">
+        <ul>
+            <li><a href="gerenciar_modelos.php">Gerenciar Modelos de Calçados</a></li>
+            <li><a href="gerenciar_variacoes.php">Gerenciar Variações de Calçados</a></li>
+            <li><a href="gerenciar_clientes.php">Gerenciar Clientes</a></li>
+            <li><a href="gerenciar_pedidos.php">Gerenciar Pedidos</a></li>
+            <li><a href="historico.php">Histórico</a></li>
+            <li><a href="logout.php">Sair</a></li>
+        </ul>
+
+    </nav>
+</body>
+
+</html>
